a47ce65b8a3e36fa3497a6c8b0bbc9f3ec3d3427
Mahamodul Hasan Moon <coddermoon@gmail.com>
2023-01-18 00:14:23 +0600

    make two reducer done

--- basicConcept/multipleReducer.js
+++ basicConcept/multipleReducer.js
@@ -5,14 +5,21 @@ const { createStore } = require("redux");
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCT = "ADD_PRODUCT";
 
-// product reducer
+const GET_CARD = 'GET_CARD';
+const ADD_CARD = 'ADD_CARD';
+
+// product reducer reducer number 1
 
 const initialProductState = {
   products: "",
   productCount: 0,
 };
 
-
+const getProducts = () => {
+  return {
+    type: GET_PRODUCTS,
+  };
+};
 
 const addProduct = (product) => {
   return {
@@ -44,16 +51,69 @@ const productReducer = (state = initialProductState, action) => {
 
 // card reducer
 
+// card state 
+
+const initialCardState = {
+    carts: "",
+    numberOfItems: 0,
+  };
+  
+
+//   actions
+
+const getCard = ()=>{
+    return {
+        type : GET_CARD,
+    }
+}
+
+const addCard = (card)=>{
+    return {
+        type : ADD_CARD,
+        payload : card,
+    }
+}
+
+// make reducer
+
+const cardReducer = (state=initialCardState, action)=>{
+
+    switch (action.type) {
+        case GET_CARD:
+            
+            return {
+                ...state
+            }
+            case ADD_CARD :
+            return {
+
+                carts :[...state.carts,action.payload],
+                numberOfItems: state.numberOfItems + 1
+
+            }
+    
+        default:
+            state
+    }
+
+}
+
+
 
 
 // store
 
-const store = createStore(productReducer)
+const store = createStore(cardReducer)
 
 // subscribe
 store.subscribe(() => {
     console.log(store.getState());
   });
 
-  store.dispatch(addProduct('mahamodul Hasan Moon'))
-  store.dispatch(addProduct('mahamodul Hasan '))
+//   store.dispatch(addProduct('mahamodul Hasan Moon'))
+//   store.dispatch(getProducts())
+//   store.dispatch(addProduct('mahamodul Hasan '))
+
+
+  store.dispatch(getCard())
+  store.dispatch(addCard('pen'))
